a45ac774f6e82028dcecc1651a2054de9c620c0d
ug-code <ugurgunes@windowslive.com>
2019-05-17 00:27:03 +0300

    Add with relation

--- src/Datatable.php
+++ src/Datatable.php
@@ -6,19 +6,21 @@ namespace Ugcode\EloquentDatatable;
 class Datatable
 {
     public static $request;
+    /** @var \Illuminate\Database\Eloquent\Builder $query */
     public static $query;
     public static $records_total;
     public static $records_filtered;
+    public static $debug = true;
 
     static function make($model)
     {
-        self::$request = $_POST;
-        self::$query = $model;
-
+        self::$request = $_REQUEST;
+        self::$query   = $model;
         self::setRecordsTotal();
         self::filter();
         self::setRecordsFiltered();
-        self::orderLimit();
+        self::order();
+        self::limit();
 
         return self::renderJson();
     }
@@ -34,9 +36,24 @@ class Datatable
     {
         if (!empty(self::$request['search']['value'])) {
             self::$query->where(function($query) {
+                /** @var \Illuminate\Database\Eloquent\Builder $query */
                 foreach (self::$request['columns'] as $column) {
                     if ($column['searchable'] == 'true') {
-                        $query->orWhere($column['name'], 'like', '%' . self::$request['search']['value'] . '%');
+
+                        if (self::isWithRelation($column)) {
+                            $rel = self::isWithRelation($column);
+                            $query->whereHas($rel[0], function($q) use ($column) {
+                                /** @var \Illuminate\Database\Eloquent\Builder $q */
+                                $q->orWhere($column['name'], 'like', '%' . self::$request['search']['value'] . '%');
+                            });
+
+                        } else {
+                            $query->orWhere($column['name'], 'like', '%' . self::$request['search']['value'] . '%');
+
+
+                        }
+
+
                     }
                 }
             });
@@ -49,14 +66,61 @@ class Datatable
         self::$records_filtered = self::$query->count();
     }
 
-    // apply order by & limit
-    static function orderLimit()
+
+    static function order()
     {
-        self::$query->orderBy(self::$request['columns'][self::$request['order'][0]['column']]['name'], self::$request['order'][0]['dir']);
+        $dt_order_column = self::$request['order'][0]['column'];
+        $dt_columns      = self::$request['columns'][$dt_order_column];
+        $dt_field        = $dt_columns['name'] ? $dt_columns['name'] : $dt_columns['data'];
+
+
+        $dt_direction = self::$request['order'][0]['dir'];
+
+
+        self::$query->orderBy($dt_field, $dt_direction);
+    }
+
+    static function isWithRelation($column)
+    {
+        $explode = explode('.', $column['data']);
+        if (isset($explode[1])) {
+            return in_array($explode[0], self::getRealationList(), true) ? $explode : false;
+
+        }
+
+        return false;
+    }
+
+    // apply order by & limit
+    static function limit()
+    {
+
         self::$query->skip(self::$request['start'])
                     ->take(self::$request['length']);
     }
 
+    static function getRealationList()
+    {
+        return (array_keys((array)self::$query->getEagerLoads()));
+
+    }
+
+    static function debug()
+    {
+        return true;
+    }
+
+    static function getSqlWithBinding($query)
+    {
+        /** @var \Illuminate\Database\Eloquent\Builder $query */
+        $sql = $query->toSql();
+        foreach ($query->getBindings() as $binding) {
+            $value = is_numeric($binding) ? $binding : '\'' . $binding . '\'';
+            $sql   = preg_replace('/\?/', $value, $sql, 1);
+        }
+
+        return $sql;
+    }
 
     // render json output
     static function renderJson()
@@ -67,10 +131,17 @@ class Datatable
         $array['recordsFiltered'] = self::$records_filtered;
         $array['data']            = [];
 
-       // pr(self::$query->toSql(), 1);
+        if (self::debug()) {
+            $array['sql'] = self::getSqlWithBinding(self::$query);
+        }
+        //print_r(self::$query->toSql());die();
 
-        $results = self::$query->get();
-       // pr($results->toArray(), 1);
+        $results = self::$query->get()
+                               ->toArray();
+
+        //                       ->toArray();
+
+        // pr($results->toArray(), 1);
         //pr(self::$query->getBindings());
         //pr(self::$query->toSql(),1);
         foreach ($results as $result) {
